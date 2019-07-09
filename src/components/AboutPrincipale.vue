60a6ed4145a169a641563aa6a4b5a567693d42d1
AndreaBUniTN <andrea.boesso@studenti.unitn.it>
2019-07-09 14:36:11 +0200

    About page update
    Added AOS.
    Updated the About page using Vue and new content.
    I'm pretty happy.

--- src/components/AboutPrincipale.vue
+++ src/components/AboutPrincipale.vue
@@ -1,28 +1,62 @@
 <template>
     <v-container>
         <v-layout column>
-            <v-flex  class="text-xs-center my-5" id="titolo">Bokiaz Clothing</v-flex>
-            <v-flex>
-                <div class="headline mt-3"><strong>STREETWEAR: ABBIGLIAMENTO E ACCESSORI DEI MIGLIORI BRAND</strong></div>
-                <p class="subheading mt-3">La selezione streetwear di Bokiaz Clothing risponde ad ogni tipo di richiesta con tutto il meglio dell'abbigliamento di strada, dai grandi classici alle uscite più esclusive. Se Nike è la regina delle scarpe da ginnastica grazie a una storia di successi che risale agli anni '70, Iuter e Octopus sono due eccellenze made in Italy con un grande seguito nel mondo mentre la linea adidas Originals x Pharrell Williams fonde sportswear e street style in una collezione unica di felpe, magliette e sneakers.Tre esempi che hanno solo grattato la superficie di un ambiente che continua a crescere: la nostra offerta unisce hip hop, il linguaggio visivo dei graffiti e l'impatto degli atleti in una pagina che racconta la scena street dal nostro punto di vista, toccando ogni angolo della cultura di strada.</p>
-            </v-flex>
-            <v-flex>
-                <div class="headline mt-3"><strong>Bokiaz Clothing, la più grande selezione online di marchi di abbigliamento e di accessori in Italia</strong></div>
-                <p class="subheading mt-3">Lo skateboard è uno dei pilastri della cultura di strada, può contare su marchi con milioni di appassionati e su capi storici, tra cui tantissime sneaker. Ispirato sia dalle atmosfere della West Coast che al cemento di New York City così come alle lunghe giornate negli skatepark, l'abbigliamento da skate è stato protagonista di una serie di rivoluzioni sociali; indossato da ribelli e creativi, è diventato il manifesto per chi vuole esprimere liberamente le proprie idee. Un nome si distingue tra gli altri, Vans: profondamente radicata nella cultura urbana di Los Angeles, l'azienda dei fratelli Van Doren ha cavalcato i movimenti giovanili degli anni '70 e '80, diffondendosi a macchia d'olio grazie a uno stile semplice e diretto. Silhouette come le Old Skool restano pietre miliari per chiunque si avvicini a questo mondo, con un effetto paragonabile solo alle One Star di Converse. Annoverato nella lista delle scarpe basse da avere a tutti i costi, queste sneaker vintage sono il lato skate di un brand camaleontico e padre delle Chuck Taylor, che nel corso della sua storia ultracentenaria ha lasciato un'impronta in ogni settore dello streetwear. Anche se ormai è cresciuto così tanto da sfuggire a una definizione precisa, perfino Stussy è partito dalla tavola prima di entrare nella leggenda grazie a un font semplice ma inconfondibile, applicato su magliette e felpe adorate in ogni stato. Siccome in Oregon sono abituati ad avere le idee chiare, Nike SB invece è partito con l'intenzione di essere da esempio per le nuove generazioni di skater: parecchie collezioni dopo, la linea da skate dello Swoosh si conferma tappa obbligata per chi vuole addentrarsi nell'essenza più profonda di questa disciplina e capirne i segreti nascosti. Per concludere, due pesi massimi come Huf e Obey tengono alto il nome delle magliette logo e degli snapback, esportando l'energia di San Francisco e il forte impatto grafico delle opere di Shepard Fairey in tutto il mondo. Un cappellino girato al contrario, la tua tavola sotto l'ultimo paio di sneakers da skate e il vento in faccia: questa è la vera skate life!</p>
-            </v-flex>
-            <v-flex>
-                <div class="headline mt-3"><strong>Funzionalità al passo con la moda</strong></div>
-                <p class="subheading mt-3">
-                    Per prima cosa, l'abbigliamento deve essere funzionale e resistente, per durare nel tempo e garantire la protezione necessaria. Da qui il successo del workwear: la transizione dalle uniformi da lavoro al lifestyle è stata naturale per marchi come Carhartt, abituati a produrre grandi quantità di divise per le fabbriche attorno a Detroit. La compagnia a stelle e strisce non ha fatto altro che coniugare praticità e resistenza con fit più morbidi, facendo arrivare in strada quello che veniva indossato dentro le industrie: modelli come la Chore Coat, la Car-Lux e i pantaloni Carhartt Aviation Ripstop hanno fatto la fortuna di un brand che negli anni '90 è diventato sinonimo di freshness grazie alla musica rap e hip hop, portato da icone del calibro di Tupac Shakur e Notorious Big. Ecco da dove arrivano le giacche anorak con maxi-tasca centrale, i pantaloni cargo e le felpe oversize con il cappuccio: il workwear oggi è una filosofia di vita che mescola la qualità dei tessuti con il background, per uno stile che si riconosce a colpo d'occhio. Per restare in tema, Levi's ha iniziato tutto con un blue denim di ottima fattura e ci costruito sopra un impero: i Levi's 501 sono il paio di pantaloni jeans più iconico della storia e hanno indicato la via ad un'azienda che ora può contare su un logo unico e su un'offerta bilanciata che arriva fino alle immancabili giacche di jeans. Qualunque cosa succeda, il denim non andrà mai fuori moda.
-                </p>
+            <v-flex class="text-xs-center my-5" id="titolo">Bokiaz Clothing</v-flex>
+            <v-layout row class="myFade my-5" data-aos="fade-down" v-for="item in items" :key="item.title">
+                <v-layout v-show="!item.imgPos">
+                    <v-flex xs10>
+                        <v-parallax height="300" :src="item.img"></v-parallax>
+                    </v-flex>
+                    <v-flex xs2></v-flex>
+                </v-layout>
+                <v-flex xs8>
+                    <div class="headline"><strong></strong>{{ item.title}}</div>
+                    <p class="subheading mt-4">{{ item.text }}</p>
                 </v-flex>
+                <v-layout v-show="item.imgPos">
+                    <v-flex xs2></v-flex>
+                    <v-flex xs10>
+                        <v-parallax height="300" :src="item.img"></v-parallax>
+                    </v-flex>
+                </v-layout>
+            </v-layout>
         </v-layout>
     </v-container>
 </template>
 
 <script>
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 export default {
-    name: 'AboutPrincipale'
+    name: 'AboutPrincipale',
+    data: function() {
+        return {
+            items: [
+                {
+                    title: "STREETWEAR: ABBIGLIAMENTO E ACCESSORI DEI MIGLIORI BRAND",
+                    text: "La selezione streetwear di Bokiaz Clothing risponde ad ogni tipo di richiesta con tutto il meglio dell'abbigliamento di strada, dai grandi classici alle uscite più esclusive. Se Nike è la regina delle scarpe da ginnastica grazie a una storia di successi che risale agli anni '70, Iuter e Octopus sono due eccellenze made in Italy con un grande seguito nel mondo mentre la linea adidas Originals x Pharrell Williams fonde sportswear e street style in una collezione unica di felpe, magliette e sneakers.Tre esempi che hanno solo grattato la superficie di un ambiente che continua a crescere: la nostra offerta unisce hip hop, il linguaggio visivo dei graffiti e l'impatto degli atleti in una pagina che racconta la scena street dal nostro punto di vista, toccando ogni angolo della cultura di strada.",
+                    img: "https://cdn.vuetifyjs.com/images/parallax/material.jpg",
+                    imgPos: true
+                },
+                {
+                    title: "Bokiaz Clothing, la più grande selezione online di marchi di abbigliamento e di accessori in Italia",
+                    text: "Lo skateboard è uno dei pilastri della cultura di strada, può contare su marchi con milioni di appassionati e su capi storici, tra cui tantissime sneaker. Ispirato sia dalle atmosfere della West Coast che al cemento di New York City così come alle lunghe giornate negli skatepark, l'abbigliamento da skate è stato protagonista di una serie di rivoluzioni sociali; indossato da ribelli e creativi, è diventato il manifesto per chi vuole esprimere liberamente le proprie idee. Un nome si distingue tra gli altri, Vans: profondamente radicata nella cultura urbana di Los Angeles, l'azienda dei fratelli Van Doren ha cavalcato i movimenti giovanili degli anni '70 e '80, diffondendosi a macchia d'olio grazie a uno stile semplice e diretto. Silhouette come le Old Skool restano pietre miliari per chiunque si avvicini a questo mondo, con un effetto paragonabile solo alle One Star di Converse. Per concludere, due pesi massimi come Huf e Obey tengono alto il nome delle magliette logo e degli snapback, esportando l'energia di San Francisco e il forte impatto grafico delle opere di Shepard Fairey in tutto il mondo. Un cappellino girato al contrario, la tua tavola sotto l'ultimo paio di sneakers da skate e il vento in faccia: questa è la vera skate life!",
+                    img: "https://cdn.vuetifyjs.com/images/parallax/material.jpg",
+                    imgPos: false
+                },
+                {
+                    title: "Funzionalità al passo con la moda",
+                    text: "Per prima cosa, l'abbigliamento deve essere funzionale e resistente, per durare nel tempo e garantire la protezione necessaria. Da qui il successo del workwear: la transizione dalle uniformi da lavoro al lifestyle è stata naturale per marchi come Carhartt, abituati a produrre grandi quantità di divise per le fabbriche attorno a Detroit. La compagnia a stelle e strisce non ha fatto altro che coniugare praticità e resistenza con fit più morbidi, facendo arrivare in strada quello che veniva indossato dentro le industrie: modelli come la Chore Coat, la Car-Lux e i pantaloni Carhartt Aviation Ripstop hanno fatto la fortuna di un brand che negli anni '90 è diventato sinonimo di freshness grazie alla musica rap e hip hop, portato da icone del calibro di Tupac Shakur e Notorious Big. Ecco da dove arrivano le giacche anorak con maxi-tasca centrale, i pantaloni cargo e le felpe oversize con il cappuccio: il workwear oggi è una filosofia di vita che mescola la qualità dei tessuti con il background, per uno stile che si riconosce a colpo d'occhio. Per restare in tema, Levi's ha iniziato tutto con un blue denim di ottima fattura e ci costruito sopra un impero: i Levi's 501 sono il paio di pantaloni jeans più iconico della storia e hanno indicato la via ad un'azienda che ora può contare su un logo unico e su un'offerta bilanciata che arriva fino alle immancabili giacche di jeans. Qualunque cosa succeda, il denim non andrà mai fuori moda.",
+                    img: "https://cdn.vuetifyjs.com/images/parallax/material.jpg",
+                    imgPos: true
+                }
+            ]
+        };
+    },
+    created() {
+        AOS.init();
+    }
 };
 </script>
 
@@ -37,4 +71,5 @@ export default {
   font-size: 70px;
   text-decoration:none;
 }
+
 </style>
--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,41 +1,46 @@
 <template>
     <span>
+      <div style="height:64px"></div>
     <v-container class = "containerProdotto" grid-list-lg>
-        <div style="height:64px"></div>
+        
         <div class="left-column">
             <v-img class="left-column img" :src='clothData.imglink'/>
         </div>
         <div class="right-column">
-            <div class="product-description">
-              <span>{{ clothData.category }}</span>
-              <h1>{{ clothData.name }}</h1>
-              <p>{{ clothData.desc }}</p>
+          <div class="product-description">
+            <span>{{ clothData.category }}</span>
+            <h1>{{ clothData.name }}</h1>
+            <p>{{ clothData.desc }}</p>
+          </div>
+          <div class="product-configuration">
+            <div class="product-color">
+              <span>Valuta questo prodotto:</span>
+                <v-rating
+                      v-model="rating"
+                      color="yellow darken-3"
+                      background-color="grey darken-1"
+                      empty-icon="$vuetify.icons.ratingEmpty"
+                      hover
+                      :readonly="this.rated"
+                      half-increments
+                >
+                </v-rating>
+                <label v-show="this.rated">{{ sentence }}</label>
             </div>
-            <div class="product-configuration">
-                <div class="product-color">
-                    <span>Valuta questo prodotto:</span>
-                    <v-rating
-                        v-model="rating"
-                        color="yellow darken-3"
-                        background-color="grey darken-1"
-                        empty-icon="$vuetify.icons.ratingEmpty"
-                        hover
-                        :readonly="this.rated"
-                        half-increments
-                      >
-                    </v-rating>
-                    <label v-show="this.rated">{{ sentence }}</label>
-                  </div>
-                </div>
+          </div>
 
-                <div class="product-price">
-                  <span>{{ clothData.price }}€</span>
-                  <a href="#" class="cart-btn" color="#222226">ACQUISTA ORA</a>
-                </div>
-            </div>
+              <div class="product-price">
+                <span>{{ clothData.price }}€</span>
+                <a target="_blank" :href="clothData.link" class="cart-btn" color="#222226">ACQUISTA ORA</a>
+              </div>
+        </div>
     </v-container>
+    <!--<div style="height:32vh"></div> Possible solution
     <div class="text-xs-center">
-      <v-btn round color="#222226" class="endBtn"  @click="$router.go(-1)" dark>Indietro</v-btn>
+      <v-btn round color="#222226" class="endBtn" //93vh// style="position:relative" @click="$router.go(-1)" dark>Indietro</v-btn>
+    </div>-->
+    <div class="text-xs-center">
+      <v-btn round color="#222226" class="endBtn" @click="$router.go(-1)" dark>Indietro</v-btn>
     </div>
     </span>
 </template>
@@ -191,5 +196,33 @@ export default {
 </script>
 
 <style scoped>
-   html, body{height:100%;width:100%;margin:0;font-family:'Roboto', sans-serif;}.containerProdotto{max-width:1200px;margin:auto;padding:15px;display:flex;}.left-column{width:60%;position:relative;}.right-column{width:40%;margin-top:60px;}.left-column img{width:100%;position:absolute;}.product-description{border-bottom:1px solid #E1E8EE;margin-bottom:20px;}.product-description span{font-size:12px;color:#358ED7;letter-spacing:1px;text-transform:uppercase;text-decoration:none;}.product-description h1{font-weight:300;font-size:52px;color:#43484D;letter-spacing:-2px;}.product-description p{font-size:14px;font-weight:300;color:#86939E;line-height:24px;}.product-color span,.cable-config span{font-size:14px;font-weight:400;color:#86939E;margin-bottom:20px;display:inline-block;}.product-color{margin-bottom:30px;}.color-choose div{display:inline-block;}.cable-choose{margin-bottom:20px;}.cable-choose button{border:2px solid #E1E8EE;border-radius:6px;padding:13px 20px;font-size:14px;color:#5E6977;background-color:#fff;cursor:pointer;transition:all .5s;}.cable-choose button:hover,.cable-choose button:active,.cable-choose button:focus{border:2px solid #86939E;outline:none;}.cable-config{border-bottom:1px solid #E1E8EE;margin-bottom:20px;}.cable-config a{color:#358ED7;text-decoration:none;font-size:12px;position:relative;margin:10px 0;display:inline-block;}.cable-config a:before{content:"?";height:15px;width:15px;border-radius:50%;border:2px solid rgba(53, 142, 215, 0.5);display:inline-block;text-align:center;line-height:16px;opacity:0.5;margin-right:5px;}.product-price{display:flex;align-items:center;}.product-price span{font-size:26px;font-weight:300;color:#43474D;margin-right:20px;}.cart-btn{display:inline-block;background-color:#7DC855;border-radius:6px;font-size:16px;color:#FFFFFF;text-decoration:none;padding:12px 30px;transition:all .5s;}.cart-btn:hover{background-color:#64af3d;}@media (max-width:940px){.container{flex-direction:column;margin-top:60px;} .left-column, .right-column{width:100%;} .left-column img{width:300px;right:0;top:-65px;left:initial;}}@media (max-width:535px){.left-column img{width:220px;top:-85px;}} .endBtn{position:fixed;bottom:64px;margin:0;}
+   html, body{height:100%;width:100%;margin:0;font-family:'Roboto', sans-serif;}
+   .containerProdotto{max-width:1200px;margin:auto;padding:15px;display:flex;}
+   .left-column{width:60%;position:relative;}
+   .right-column{width:40%;margin-top:60px;}
+   .left-column img{width:100%;position:absolute;}
+   .product-description{border-bottom:1px solid #E1E8EE;margin-bottom:20px;}
+   .product-description span{font-size:12px;color:#358ED7;letter-spacing:1px;text-transform:uppercase;text-decoration:none;}
+   .product-description h1{font-weight:300;font-size:52px;color:#43484D;letter-spacing:-2px;}
+   .product-description p{font-size:14px;font-weight:300;color:#86939E;line-height:24px;}
+   .product-color span,.cable-config span{font-size:14px;font-weight:400;color:#86939E;margin-bottom:20px;display:inline-block;}
+   .product-color{margin-bottom:30px;}
+   .color-choose div{display:inline-block;}.cable-choose{margin-bottom:20px;}
+   .cable-choose button{border:2px solid #E1E8EE;border-radius:6px;padding:13px 20px;font-size:14px;color:#5E6977;background-color:#fff;cursor:pointer;transition:all .5s;}
+   .cable-choose button:hover,.cable-choose button:active,.cable-choose button:focus{border:2px solid #86939E;outline:none;}
+   .cable-config{border-bottom:1px solid #E1E8EE;margin-bottom:20px;}
+   .cable-config a{color:#358ED7;text-decoration:none;font-size:12px;position:relative;margin:10px 0;display:inline-block;}
+   .cable-config a:before{content:"?";height:15px;width:15px;border-radius:50%;border:2px solid rgba(53, 142, 215, 0.5);display:inline-block;text-align:center;line-height:16px;opacity:0.5;margin-right:5px;}
+   .product-price{display:flex;align-items:center;}
+   .product-price span{font-size:26px;font-weight:300;color:#43474D;margin-right:20px;}
+   .cart-btn{display:inline-block;background-color:#7DC855;border-radius:6px;font-size:16px;color:#FFFFFF;text-decoration:none;padding:12px 30px;transition:all .5s;}
+   .cart-btn:hover{background-color:#64af3d;}
+   @media (max-width:940px){
+     .container{flex-direction:column;margin-top:60px;} 
+     .left-column, .right-column{width:100%;} 
+     .left-column img{width:300px;right:0;top:-65px;left:initial;}
+     }
+    @media (max-width:535px)
+    {.left-column img{width:220px;top:-85px;}}
+     .endBtn{position:fixed;bottom:64px;margin:0;}
 </style>
